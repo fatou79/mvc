dcb67e36c997d2addd473d47e67b76283a90dc64
coumba <diagne@gmail.com>
2021-09-15 23:19:26 +0000

    MVC

---
+++ src/main/java/com/fatou/controller/LoginService.java
@@ -0,0 +1,14 @@
+package com.fatou.controller;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+
+public class LoginService {
+	public boolean validateUser(String user, String password) {
+		return user.equalsIgnoreCase("diop") && password.equals("diagne");
+	}
+
+
+}
